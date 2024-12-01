392207ef59d526fe11b32e96e33887a55d5b3c85
ahmad-573 <amahmood@student.ethz.ch>
2024-12-01 19:28:46 +0100

    linear cases pass

--- code/verifier.py
+++ code/verifier.py
@@ -20,11 +20,15 @@ def analyze(
     """
     res = True
     # go through all the layers
-    lower_bound = torch.maximum(inputs - eps, torch.zeros_like(inputs))
-    upper_bound = torch.minimum(inputs + eps, torch.ones_like(inputs))
+    lower_bound_first = torch.maximum(inputs - eps, torch.zeros_like(inputs))
+    upper_bound_first = torch.minimum(inputs + eps, torch.ones_like(inputs))
+    # lower_bound_first = inputs - eps
+    # upper_bound_first = inputs + eps
+    # lower_bound = torch.zeros(1, 2) - 1
+    # upper_bound = torch.zeros(1, 2) + 1
 
-    lower_bound = lower_bound.flatten().view(1, -1)
-    upper_bound = upper_bound.flatten().view(1, -1)
+    lower_bound = lower_bound_first.flatten().view(1, -1)
+    upper_bound = upper_bound_first.flatten().view(1, -1)
 
     low_relational = lower_bound.clone()
     up_relational = upper_bound.clone()
@@ -42,6 +46,30 @@ def analyze(
             lower_bound = lower_bound_new.T
             upper_bound = upper_bound_new.T
     
+
+    # backsubstitute the bounds (only for linear at this point)
+    coeffs = torch.eye(inputs.shape[-1] * inputs.shape[-2])
+    for i, layer in enumerate(net):
+        if layer.__class__.__name__ == "Linear":
+            constant = torch.zeros(layer.weight.shape[1], 1)
+            break
+    
+    for i, layer in enumerate(net):
+        if layer.__class__.__name__ == "Linear":
+            coeffs = layer.weight @ coeffs
+            constant = layer.weight @ constant + layer.bias.view(-1, 1) 
+    
+    coeffs_positive = torch.maximum(coeffs, torch.zeros_like(coeffs))
+    coeffs_negative = torch.minimum(coeffs, torch.zeros_like(coeffs))
+
+    lower_bound_back = coeffs_positive @ lower_bound_first.flatten().view(-1, 1) + coeffs_negative @ upper_bound_first.flatten().view(-1, 1) + constant
+    upper_bound_back = coeffs_positive @ upper_bound_first.flatten().view(-1, 1) + coeffs_negative @ lower_bound_first.flatten().view(-1, 1) + constant
+
+    # print(lower_bound.shape, upper_bound.shape, lower_bound_back.shape, upper_bound_back.shape)
+
+    lower_bound = torch.maximum(lower_bound, lower_bound_back.T)
+    upper_bound = torch.minimum(upper_bound, upper_bound_back.T)
+
     # check if lower bound for true label is greater than upper bound for other labels
     for i in range(10):
         if i != true_label:
@@ -103,6 +131,20 @@ def main():
         num_class=num_class,
         weight_path=f"models/{dataset}_{args.net}.pt",
     ).to(DEVICE)
+    # net = torch.nn.Sequential(
+    #     torch.nn.Linear(2, 3),
+    #     torch.nn.Linear(3, 1)
+    # )
+    # # init net
+    # net[0].weight = torch.nn.Parameter(torch.tensor([[2.0, -1.0], [1.0, -3.0], [1.0, 2.0]]))
+    # net[0].bias = torch.nn.Parameter(torch.tensor([1.0, 3.0, 5.0]))
+    # net[1].weight = torch.nn.Parameter(torch.tensor([[1.0, -1.0, 2.0]]))
+    # net[1].bias = torch.nn.Parameter(torch.tensor([0.0]))
+    
+    # image = torch.ones(1,2)
+    # out = net(image)
+    # analyze(net, image, 0.1, 0)
+    # print(net)
 
     image = image.to(DEVICE)
     out = net(image.unsqueeze(0))
